c10451f3393609d322bd53c854fff42f30afc48f
ruubeengc <gonzalez.147878@e.unavarra.es>
2024-05-16 10:44:59 +0200

    He conseguido que no de errores y he aplicado el partitioner, creo que separa bien, pero sigue sin escribir todos los datos...

--- src/main/java/com/mycompany/p22_mapreduce/P22_MapReduce.java
+++ src/main/java/com/mycompany/p22_mapreduce/P22_MapReduce.java
@@ -11,6 +11,8 @@ import java.io.*;
 import java.security.PrivilegedExceptionAction;
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.conf.*;
@@ -28,24 +30,42 @@ public class P22_MapReduce {
 //        private static final long UNO = 1L;
         public void map(LongWritable key, Text value, Context context) {
             try {
-                // Tengo comprobado que separa bien la regex
                 String[] str = value.toString().split(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))", -2); // --> Regex para no coger las comas que están dentro de unas comillas 
                 // El -2 indica que se incluirán todos los elementos del texto, incluso si hay campos vacíos al final.
                 String subject = str[2];
 
-                if (!(subject.equals("subject"))) { // Para saltarnos el valor literal "subject" de la primera línea
-//                    outTuple.setCount(UNO);
+//                if (!(subject.equals("subject"))) { // Para saltarnos el valor literal "subject" de la primera línea
+////                    outTuple.setCount(UNO);
+////                    String[] date = str[3].split(", ");
+////                    
+////                    System.out.println(yearInt);
+////                    outTuple.setYear(yearInt);
 //                    String[] date = str[3].split(", ");
-//                    
-//                    System.out.println(yearInt);
-//                    outTuple.setYear(yearInt);
-                    String[] date = str[3].split(", ");
-                    String year = date[1].replace("\"", "");
-                    int yearInt = Integer.parseInt(year);
-                    context.write(new Text(subject), new IntWritable(yearInt));
+//                    String year = date[1].replace("\"", "");
+//                    int yearInt = Integer.parseInt(year);
+//                    context.write(new Text(subject), new IntWritable(yearInt));
+//                }
+                if (!subject.equals("subject")) {
+                    String[] date = str[3].split("-|, ");
+                    String year = date[date.length - 1].replace("\"", "");
+
+                    // Regex para extraer el año de la fecha
+                    String regex = "\\d{4}|\\d{2}$";
+                    Pattern pattern = Pattern.compile(regex);
+
+                    // Extracción del año de la fecha y conversión al formato numérico
+                    Matcher matcher = pattern.matcher(year);
+                    if (matcher.find()) {
+                        String yearMatch = matcher.group();
+                        if (yearMatch.length() == 2) {
+                            yearMatch = "20" + yearMatch;
+                        }
+                        int yearInt = Integer.parseInt(yearMatch);
+                        context.write(new Text(subject), new IntWritable(yearInt));
+                    }
                 }
             } catch (Exception e) {
-                System.err.println(value.toString());
+//                System.err.println(value.toString()); --> Esto era para comprobar y saber donde estaba el error
                 e.printStackTrace();
             }
         }
@@ -72,8 +92,8 @@ public class P22_MapReduce {
 //                    String subject = datos[0];
 
                 }
-                System.out.println("Suma es:" + sum);
                 System.out.println("La key es: " + key.toString());
+                System.out.println("Y su suma es:" + sum);
                 context.write(key, new IntWritable(sum)); // --> No escribe todas las keys
 //                
 //                int sum = 0;
@@ -87,26 +107,27 @@ public class P22_MapReduce {
         }
     }
 
-//    public static class CaderPartitioner extends Partitioner<Text, IntWritable> {
-//
-//        @Override
-//        public int getPartition(Text key, IntWritable value, int numReduceTasks) {
-//            // Entrada partitioner: los datos completos en una colección de pares clave-valor.
-//            
-//            
-//            if (numReduceTasks == 0) {
-//                return 0;
-//            }
-//            // "Partimos" los datos según los criterios de edad que se nos idican:
-//            if (year <= 2015) {
-//                return 0;
-//            } else if (year > 2015 && year <= 2016) {
-//                return 1 ;
-//            } else {
-//                return 2 ;
-//            }
-//        }
-//    }
+    public static class CaderPartitioner extends Partitioner<Text, IntWritable> {
+
+        @Override
+        public int getPartition(Text key, IntWritable value, int numReduceTasks) {
+            // Entrada partitioner: los datos completos en una colección de pares clave-valor.
+            int year = Integer.parseInt(value.toString());
+            
+            if (numReduceTasks == 0) {
+                return 0;
+            }
+            // "Partimos" los datos según los criterios de edad que se nos idican:
+            if (year <= 2015) {
+                return 0;
+            } else if (year > 2015 && year <= 2016) {
+                return 1 ;
+            } else {
+                return 2 ;
+            }
+        }
+    }
+    
     public static void main(String[] args) {
 // Establecemos las configuraciones correspondientes añadiendo las de los partitioners:
         UserGroupInformation ugi = UserGroupInformation.createRemoteUser("a_83026");
@@ -125,14 +146,14 @@ public class P22_MapReduce {
                     job.setMapOutputValueClass(IntWritable.class);
 
                     //set partitioner statement
-//                    job.setPartitionerClass(CaderPartitioner.class);
-//                    job.setNumReduceTasks(1);
-//                    job.setInputFormatClass(TextInputFormat.class);
-//                    job.setOutputFormatClass(TextOutputFormat.class);
-//                    job.setOutputKeyClass(Text.class);
-//                    job.setOutputValueClass(IntWritable.class);
+                    job.setPartitionerClass(CaderPartitioner.class);
+                    job.setNumReduceTasks(3);
+                    job.setInputFormatClass(TextInputFormat.class);
+                    job.setOutputFormatClass(TextOutputFormat.class);
+                    job.setOutputKeyClass(Text.class);
+                    job.setOutputValueClass(IntWritable.class);
                     FileInputFormat.addInputPath(job, new Path("/PCD2024/a_83026/DatosNews"));
-                    FileOutputFormat.setOutputPath(job, new Path("/PCD2024/a_83026/DatosNews_SalidaFechasParticionado4"));
+                    FileOutputFormat.setOutputPath(job, new Path("/PCD2024/a_83026/DatosNews_SalidaFechasParticionado"));
                     boolean finalizado = job.waitForCompletion(true);
                     System.out.println("Finalizado: " + finalizado);
                     return null;
